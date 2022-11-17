54981391b35a73b13fc61823534dcbb0a5bbc036
cauehenrique <caue313@gmail.com>
2022-11-17 20:58:10 -0300

    refactor: add typescript to the entire project

--- src/screens/Colecoes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Colecoes() {
-  return (
-    <div>Colecoes</div>
-  )
-}
--- src/screens/Inicio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Inicio() {
-  return (
-    <div className='bg-sky-200'>
-      inicio
-    </div>
-  )
-}
